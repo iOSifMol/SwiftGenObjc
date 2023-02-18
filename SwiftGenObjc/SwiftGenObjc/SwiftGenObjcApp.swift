50b3c651670aaa83577dc5b768f4eb63d5b68208
iOSifMol <>
2023-02-18 21:07:15 +0200

    Initial commit

---
+++ SwiftGenObjc/SwiftGenObjc/SwiftGenObjcApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftGenObjcApp.swift
+//  SwiftGenObjc
+//
+//  Created by Iosif Moldovan on 18.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftGenObjcApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
