9320e0bd01c38de0ca9a06e40cbdacb1f0a2aa23
iOSifMol <>
2023-02-19 14:20:07 +0200

    Add Swiftgen & resourses

--- SwiftGenObjc/SwiftGenObjc/ContentView.swift
+++ SwiftGenObjc/SwiftGenObjc/ContentView.swift
@@ -10,10 +10,10 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
+          Asset.Images.earth.swiftUIImage
+            .scaledToFit()
             Text("Hello, world!")
+            .foregroundColor(Asset.Colors.textColor.swiftUIColor)
         }
         .padding()
     }
